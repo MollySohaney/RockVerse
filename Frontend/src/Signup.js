ca05424d3d16c0e810071fe1ee9fc19fa74196b3
Molly Sohaney <molly.sohaney@gmail.com>
2025-02-09 03:11:42 -0500

    frontend for signup done

--- Frontend/src/Signup.js
+++ Frontend/src/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Header from "./Header";
 
 function Signup() {
     const [username, setUsername] = useState("");
@@ -24,12 +25,15 @@ function Signup() {
     };
 
     return (
-        <div class="signup-form">
-            <h3>Sign Up</h3>
-            <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleSignup}>Sign Up</button>
+        <div>
+            <Header />
+            <div class="signup-form">
+                <h3>Sign Up</h3>
+                <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+                <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={handleSignup}>Sign Up</button>
+            </div>
         </div>
     );
 }
